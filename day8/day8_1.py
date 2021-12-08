9c836ecc520abc832e51f712d4a904ba4b20e402
dolatapatryk <dolata.patryk1@gmail.com>
2021-12-08 15:28:38 +0100

    parse input - day8

--- day8/day8_1.py
+++ day8/day8_1.py
@@ -1,6 +1,27 @@
+# segments:
+# 0 - 6
+# 1 - 2
+# 2 - 5
+# 3 - 5
+# 4 - 4
+# 5 - 5
+# 6 - 6
+# 7 - 3
+# 8 - 7
+# 9 - 6
+# unique - 1, 4, 7, 8
+
 file = open('input2.txt', 'r')
 
 lines = [line.strip() for line in file.readlines()]
-print(lines)
+
+X = []
+for line in lines:
+    splitted = line.split(' | ')
+    signal = splitted[0]
+    output = splitted[1]
+    X.append((signal, output))
+
+print(X[0])
 
 file.close()
