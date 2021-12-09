fd6d3382801b585e999f98c882446f7cb3595d0e
dolatapatryk <dolata.patryk1@gmail.com>
2021-12-09 18:08:15 +0100

    completed day9

--- day9/day9_1.py
+++ day9/day9_1.py
@@ -1,4 +1,4 @@
-file = open('input.txt', 'r')
+file = open('input2.txt', 'r')
 
 lines = [line.strip() for line in file.readlines()]
 
@@ -7,10 +7,10 @@ heightmap = {(i, j):int(val) for i, line in enumerate(lines) for j, val in enume
 result = 0
 for i, j in heightmap.keys():
     val = heightmap[(i, j)]
-    top = heightmap.get((i-1, j), 100)
-    bottom = heightmap.get((i+1, j), 100)
-    left = heightmap.get((i, j-1), 100)
-    right = heightmap.get((i, j+1), 100)
+    top = heightmap.get((i-1, j), 9)
+    bottom = heightmap.get((i+1, j), 9)
+    left = heightmap.get((i, j-1), 9)
+    right = heightmap.get((i, j+1), 9)
     if val < top and val < bottom and val < left and val < right:
         result += val+1
 print('result =', result)
--- day9/day9_2.py
+++ day9/day9_2.py
@@ -1,22 +1,34 @@
+from functools import reduce
+
 def get_low_points(heightmap):
     low_points = []
     for i, j in heightmap.keys():
         val = heightmap[(i, j)]
-        top = heightmap.get((i-1, j), 100)
-        bottom = heightmap.get((i+1, j), 100)
-        left = heightmap.get((i, j-1), 100)
-        right = heightmap.get((i, j+1), 100)
-        if val < top and val < bottom and val < left and val < right:
+        top, bottom, left, right = get_neighbours((i, j), heightmap)
+        if val < top[1] and val < bottom[1] and val < left[1] and val < right[1]:
             low_points.append((i, j))
     return low_points
 
-def get_basin(point):
+def get_neighbours(point, heightmap):
     i, j = point
-    basin = []
+    top = heightmap.get((i-1, j), 9)
+    bottom = heightmap.get((i+1, j), 9)
+    left = heightmap.get((i, j-1), 9)
+    right = heightmap.get((i, j+1), 9)
+    return (((i-1, j), top), ((i+1, j), bottom), ((i, j-1), left), ((i, j+1), right))
+
+def get_basin(point, heightmap, basin):
+    neighbours = get_neighbours(point, heightmap)
+    for neighbour in neighbours:
+        position, val = neighbour
+        if val == 9 or position in basin:
+            continue
+        basin.append(position)
+        get_basin(position, heightmap, basin)
 
     return basin
 
-file = open('input2.txt', 'r')
+file = open('input.txt', 'r')
 
 lines = [line.strip() for line in file.readlines()]
 
@@ -26,11 +38,12 @@ low_points = get_low_points(heightmap)
 
 basins = []
 for point in low_points:
-    basin = get_basin(point)
+    basin = get_basin(point, heightmap, [])
     basins.append(basin)
 
 basins.sort(key=lambda x: len(x), reverse=True)
-print(basins)
+result = reduce((lambda x, y: x * y), [len(x) for x in basins[:3]])
+print('result =', result)
         
 
 file.close()
