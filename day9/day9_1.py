da17bbaa4b71bc321b066ef66836af06259f16e0
dolatapatryk <dolata.patryk1@gmail.com>
2021-12-09 07:43:52 +0100

    completed day9 task1

--- day9/day9_1.py
+++ day9/day9_1.py
@@ -1,3 +1,19 @@
-file = open('input2.txt', 'r')
+file = open('input.txt', 'r')
+
+lines = [line.strip() for line in file.readlines()]
+
+heightmap = {(i, j):int(val) for i, line in enumerate(lines) for j, val in enumerate(line)}
+
+result = 0
+for i, j in heightmap.keys():
+    val = heightmap[(i, j)]
+    top = heightmap.get((i-1, j), 100)
+    bottom = heightmap.get((i+1, j), 100)
+    left = heightmap.get((i, j-1), 100)
+    right = heightmap.get((i, j+1), 100)
+    if val < top and val < bottom and val < left and val < right:
+        result += val+1
+print('result =', result)
+        
 
 file.close()
