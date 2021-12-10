8ef5fd19d396f4dc5699fa27d6bed5e1b2ff7716
dolatapatryk <dolata.patryk1@gmail.com>
2021-12-10 08:27:27 +0100

    completed day10 task1

--- day10/day10_1.py
+++ day10/day10_1.py
@@ -1,6 +1,38 @@
-file = open('input2.txt', 'r')
+from collections import deque
 
-lines = [line.strip() for line in file.readlines()]
-print(lines)
+file = open('input.txt', 'r')
+
+res = 0
+for line in file.readlines():
+    line = line.strip()
+    openers = deque()
+    for c in line:
+        if c in ['(', '[', '{', '<']:
+            openers.append(c)
+        elif c == ')':
+            if openers[-1] != '(':
+                res += 3
+                break
+            else:
+                openers.pop()
+        elif c == ']':
+            if openers[-1] != '[':
+                res += 57
+                break
+            else:
+                openers.pop()
+        elif c == '}':
+            if openers[-1] != '{':
+                res += 1197
+                break
+            else:
+                openers.pop()
+        elif c == '>':
+            if openers[-1] != '<':
+                res += 25137
+                break
+            else:
+                openers.pop()
+print(res)
 
 file.close()
